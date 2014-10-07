75b4ed8a499d117bf0795886209dc110dc8937ff
Anthony Kang <akang3894@gmail.com>
2014-10-06 20:00:26 -0400

    character and base_item created

---
+++ ingame_objects/Item.java
@@ -0,0 +1,9 @@
+public class Item{
+	private int x_map; // x coordinate on the map
+	private int y_map; // y coordinate on the map
+	private int screen_x; // x coordinate on the screen
+	private int screen_y; // y coordinate on the screen
+	private String image_name;
+
+
+}
