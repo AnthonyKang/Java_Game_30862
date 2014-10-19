39b99a3d78e77e29e8698eb9787dec7e835b9c72
James Tay <the_viper_90@pal-nat184-165-094.itap.purdue.edu>
2014-10-19 17:52:47 -0400

    Main game class with methods descriptions

---
+++ Game.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import javax.swing.JFrame;
+
+public class Game extends JFrame {
+// Contains:
+// 1) Player character
+// 2) List of monsters
+// 3) List of in game items
+// 4) List of player bullets
+// 5) List of monster bullets
+// 6) Game score
+
+
+
+
+
+
+
+// Game loop
+// 1) Update locations of map
+// 2) Update locations of objects
+// 3) Check collisions
+//     a) Player with monster bullets
+//     b) Player with in-game objects
+//     c) Monsters with player bullets
+// 4) Create sounds
+// 5) Update statuses
+// 6) Redraw
+
+
+}
